1ce30a4419149614c0165cc5eb0832fda518469f
pauld <paul.dragoslav@gmail.com>
2020-01-13 20:07:40 +0000

    Upgrade to Spring-boot 2.2 and JUnit 5

--- src/test/java/com/example/paul/rabbitmq/ApplicationTest.java
+++ src/test/java/com/example/paul/rabbitmq/ApplicationTest.java
@@ -1,18 +1,18 @@
 package com.example.paul.rabbitmq;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.concurrent.TimeUnit;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class ApplicationTest {
+class ApplicationTest {
 
 	@MockBean
 	private Runner runner;
@@ -24,7 +24,7 @@ public class ApplicationTest {
 	private Receiver receiver;
 
 	@Test
-	public void test() throws Exception {
+	void sendAndReceiveMessage() throws Exception {
 		rabbitTemplate.convertAndSend(Application.queueName, "Hello from RabbitMQ!");
 		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
 	}
